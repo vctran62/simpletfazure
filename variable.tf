8b3c6c65264e37a521e2eb588141ac66b0f3f0c1
Vinh Tran <vctran62@users.noreply.github.com>
2022-04-11 12:34:32 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,10 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
 }
-
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
 }
+variable subscriptionId {}
+variable clientId       {}
+variable client_secret  {}
+variable tenantId       {}
